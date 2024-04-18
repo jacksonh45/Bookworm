2b68ae213718f600688d21b0cc2ea8d1be8e3979
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-04-18 09:47:29 -0500

    finished project

---
+++ Bookworm/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  Bookworm
+//
+//  Created by Jackson Harrison on 4/15/24.
+//
+
+import SwiftUI
+
+struct Book: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Book()
+}
---
+++ Bookworm/EmptyString.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyString.swift
+//  Bookworm
+//
+//  Created by Jackson Harrison on 4/15/24.
+//
+
+import SwiftUI
+
+struct EmptyString: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyString()
+}
